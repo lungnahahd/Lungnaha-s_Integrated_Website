553c4987e934d56ad8bb6ac3598eeb2463ede7f7
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2022-02-18 21:56:32 +0900

    Feat : "VO 작성" 게시판 데이터 관리 틀 생성

---
+++ src/main/java/com/Lungnaha/IntegratedWebsite/BoardVO.java
@@ -0,0 +1,55 @@
+package com.Lungnaha.IntegratedWebsite;
+
+import java.sql.Date;
+
+// VO 클래스는 데이터 전달 목적의 객체 ==> 데이터 틀을 형성!!
+public class BoardVO {
+	private int seq;
+	private String title;
+	private String writer;
+	private String content;
+	private Date regDate;
+	private int cnt;
+	
+	// 변수를 선언하고 Getter와 Setter 메소드를 통해 관리 가능
+	
+	public int getSeq() {
+		return seq;
+	}
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+	public int getCnt() {
+		return cnt;
+	}
+	public void setCnt(int cnt) {
+		this.cnt = cnt;
+	}
+	
+	
+	
+}
