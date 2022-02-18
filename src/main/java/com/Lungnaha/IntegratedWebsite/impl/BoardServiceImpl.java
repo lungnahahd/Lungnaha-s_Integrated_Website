fa60e8e515876b4b809d26270f14f48e487cfc98
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2022-02-18 23:00:26 +0900

    Feat : "Service 구현 클래스 생성" 인터페이스를 사용한 Service 클래스 구현

--- src/main/java/com/Lungnaha/IntegratedWebsite/impl/BoardServiceImpl.java
+++ src/main/java/com/Lungnaha/IntegratedWebsite/impl/BoardServiceImpl.java
@@ -1,5 +1,42 @@
 package com.Lungnaha.IntegratedWebsite.impl;
 
-public class BoardServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Lungnaha.IntegratedWebsite.BoardService;
+import com.Lungnaha.IntegratedWebsite.BoardVO;
+
+@Service("boardService")
+public class BoardServiceImpl implements BoardService {
+	@Autowired
+	private BoardDAO boardDAO;
+		
+	@Override
+	public void insertBlogBoard(BoardVO vo) {
+		boardDAO.insertBlogBoard(vo);
+		
+	}
+
+	@Override
+	public void updateBlogBoard(BoardVO vo) {
+		boardDAO.updateBlogBoard(vo);
+	}
+
+	@Override
+	public void deleteBlogBoard(BoardVO vo) {
+		boardDAO.deleteBlogBoard(vo);
+	}
+
+	@Override
+	public BoardVO getBlogBoard(BoardVO vo) {
+		return boardDAO.getBlogBoard(vo);
+	}
+
+	@Override
+	public List<BoardVO> getlistBlogBoard(BoardVO vo) {
+		return boardDAO.getlistBlogBoard(vo);
+	}
 
 }
