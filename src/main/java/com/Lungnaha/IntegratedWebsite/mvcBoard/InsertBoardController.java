5fcc220597409363e3b8f41da3bd7ef9d058f113
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2022-02-26 21:56:06 +0900

    Feat : "글 등록 기능 재구현" Spring MVC 활용 구현

---
+++ src/main/java/com/Lungnaha/IntegratedWebsite/mvcBoard/InsertBoardController.java
@@ -0,0 +1,39 @@
+package com.Lungnaha.IntegratedWebsite.mvcBoard;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import com.Lungnaha.IntegratedWebsite.BoardService;
+import com.Lungnaha.IntegratedWebsite.BoardVO;
+
+public class InsertBoardController implements Controller {
+	AbstractApplicationContext container = new GenericXmlApplicationContext("applicationContext.xml");
+	BoardService boardService = (BoardService) container.getBean("boardService");
+	
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
+		System.out.println("MVC 활용 ==> 글 등록 구현");
+		
+		// 입력 정보 추룰
+		String title = request.getParameter("title");
+		String writer = request.getParameter("writer");
+		String content = request.getParameter("content");
+		// DB 연동 처리
+		BoardVO vo = new BoardVO();
+		vo.setTitle(title);
+		vo.setWriter(writer);
+		vo.setContent(content);
+		
+		boardService.insertBlogBoard(vo);
+		// 화면 연결
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("getBoardList.do");
+		return mav;
+		
+	}	
+}
