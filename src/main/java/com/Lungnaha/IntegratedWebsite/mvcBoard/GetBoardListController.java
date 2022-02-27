16d021e3888490eae065e71fb74ef24dca6f8809
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2022-02-27 12:13:21 +0900

    Feat : "글 목록 재구현" 어노테이션 활용

--- src/main/java/com/Lungnaha/IntegratedWebsite/mvcBoard/GetBoardListController.java
+++ src/main/java/com/Lungnaha/IntegratedWebsite/mvcBoard/GetBoardListController.java
@@ -7,33 +7,42 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
 
 import com.Lungnaha.IntegratedWebsite.BoardService;
 import com.Lungnaha.IntegratedWebsite.BoardVO;
-import com.Lungnaha.IntegratedWebsite.Impl.BoardDAO;
 
-
-public class GetBoardListController implements Controller {
+@Controller
+public class GetBoardListController  {
 	AbstractApplicationContext container = new GenericXmlApplicationContext("applicationContext.xml");
 	BoardService boardService = (BoardService) container.getBean("boardService");
 	
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
+	@RequestMapping("/getBoardList.do")
+	public ModelAndView handleRequest(BoardVO vo, ModelAndView mav) {
 		System.out.println("Spring MVC ==> 글 목록 검색 처리");
 		
+		mav.addObject("boardList", boardService.getlistBlogBoard(vo));
+		mav.setViewName("getBoardList.jsp");
+		return mav;
+		
+		
+		
+		
+		// @@@@@@@@@@@ XML 설정파일 적용 예시
 		//DB 연동 처리
-		BoardVO vo = new BoardVO();
+		//BoardVO vo = new BoardVO();
 		//BoardDAO boardDAO = new BoardDAO();
 		//List<BoardVO> boardlist = boardDAO.getlistBlogBoard(vo);
-		List<BoardVO> boardList = boardService.getlistBlogBoard(vo);
+		//List<BoardVO> boardList = boardService.getlistBlogBoard(vo);
 		
 		// 검색 결과와 맵핑해서 이를 화면에 출력
-		ModelAndView mav = new ModelAndView();
-		mav.addObject("boardList", boardList);
+		//ModelAndView mav = new ModelAndView();
+		//mav.addObject("boardList", boardList);
 		//mav.setViewName("getBoardList.jsp"); ==> ViewResolver 적용 전
-		mav.setViewName("getBoardList");
-		return mav;
+		/*
+		 * mav.setViewName("getBoardList"); return mav;
+		 */
 	}
 }
