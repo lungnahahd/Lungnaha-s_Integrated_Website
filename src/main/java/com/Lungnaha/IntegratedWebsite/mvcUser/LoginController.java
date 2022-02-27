16646197f8ebf1498fecb08786af99e894a659c8
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2022-02-27 13:09:11 +0900

    Feat : "로그인 재구현" 어노테이션 활용

--- src/main/java/com/Lungnaha/IntegratedWebsite/mvcUser/LoginController.java
+++ src/main/java/com/Lungnaha/IntegratedWebsite/mvcUser/LoginController.java
@@ -5,40 +5,50 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
 
 import com.Lungnaha.IntegratedWebsite.UserService;
 import com.Lungnaha.IntegratedWebsite.UserVO;
 
-public class LoginController implements Controller { // Spring MVC 구조로 개발을 수정 -> 기존 구조는 "DispatcherServlet"에서만 모든 걸 처리에 구조가 복잡하고 유지보수에 부적합
+@Controller
+public class LoginController { // Spring MVC 구조로 개발을 수정 -> 기존 구조는 "DispatcherServlet"에서만 모든 걸 처리에 구조가 복잡하고 유지보수에 부적합
 	
 	AbstractApplicationContext container = new GenericXmlApplicationContext("applicationContext.xml");
 	UserService userService = (UserService) container.getBean("userService");
 	
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
+	@RequestMapping("/login.do")
+	public String login(UserVO vo) {
 		System.out.println("Spring MVC 구조 ==> 로그인 처리");
-		// 사용자 입력 정보 추출
-		String id = request.getParameter("id");
-		String password = request.getParameter("password");
-		// DB 연동 처리
-		UserVO vo = new UserVO();
-		vo.setId(id);
-		vo.setPassword(password);
 		
-		UserVO user = new UserVO();
-		user = userService.getUser(vo);
-		// 결과 값을 가지고 적절한 화면에 맵핑
-		ModelAndView mav = new ModelAndView();
-		if(user != null) {
-			//mav.setViewName("getBoardList.do"); ==> ViewReslover 적용 전
-			mav.setViewName("redirect:getBoardList.do");
+		if(userService.getUser(vo)!=null) {
+			return "getBoardList.do";
 		}else {
-			//mav.setViewName("login.jsp");  ==> ViewReslover 적용 전
-			mav.setViewName("redirect:login.jsp");
+			return "login.jsp";
 		}
-		return mav;
+		
+		
+		// @@@@@@@@@@@ XML 설정파일 적용 예시
+		// 사용자 입력 정보 추출
+		/*
+		 * String id = request.getParameter("id"); String password =
+		 * request.getParameter("password");
+		 */
+		// DB 연동 처리
+		/*
+		 * UserVO vo = new UserVO(); vo.setId(id); vo.setPassword(password);
+		 * 
+		 * UserVO user = new UserVO(); user = userService.getUser(vo);
+		 */
+		// 결과 값을 가지고 적절한 화면에 맵핑
+		/*
+		 * ModelAndView mav = new ModelAndView(); if(user != null) {
+		 * //mav.setViewName("getBoardList.do"); ==> ViewReslover 적용 전
+		 * mav.setViewName("redirect:getBoardList.do"); }else {
+		 * //mav.setViewName("login.jsp"); ==> ViewReslover 적용 전
+		 * mav.setViewName("redirect:login.jsp"); } return mav;
+		 */
 		
 	}
 	
