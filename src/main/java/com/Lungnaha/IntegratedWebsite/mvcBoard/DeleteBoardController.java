bccafec769fd863527c5e2d3baf6c728b22bd353
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2022-02-27 13:02:18 +0900

    Feat : '글 삭제 재구현' 어노테이션 활용

--- src/main/java/com/Lungnaha/IntegratedWebsite/mvcBoard/DeleteBoardController.java
+++ src/main/java/com/Lungnaha/IntegratedWebsite/mvcBoard/DeleteBoardController.java
@@ -5,32 +5,44 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
 
 import com.Lungnaha.IntegratedWebsite.BoardService;
 import com.Lungnaha.IntegratedWebsite.BoardVO;
 
-public class DeleteBoardController implements Controller {
+@Controller
+public class DeleteBoardController {
 	AbstractApplicationContext container = new GenericXmlApplicationContext("applicationContext.xml");
 	BoardService boardService = (BoardService) container.getBean("boardService");
 	
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
+	@RequestMapping("/deleteBoard.do")
+	public String deleteBoard(BoardVO vo){
 		System.out.println("MVC 활용 ==> 글 삭제 구현");
 		
-		// 정보 추출
-		String seq = request.getParameter("seq");
-		// DB 연동
-		BoardVO vo = new BoardVO();
-		vo.setSeq(Integer.parseInt(seq));
-		
 		boardService.deleteBlogBoard(vo);
+		return("getBoardList.do");
+		
+		
+		
+		
+		
+		// @@@@@@@@@@@ XML 설정파일 적용 예시
+		// 정보 추출
+		//String seq = request.getParameter("seq");
+		// DB 연동
+		/*
+		 * BoardVO vo = new BoardVO(); vo.setSeq(Integer.parseInt(seq));
+		 * 
+		 * boardService.deleteBlogBoard(vo);
+		 */
 		// 화면 연결
-		ModelAndView mav = new ModelAndView();
-		//mav.setViewName("getBoardList.do");  ==> ViewReslover 적용 전
-		mav.setViewName("redirect:getBoardList.do");
-		return mav;
+		/*
+		 * ModelAndView mav = new ModelAndView(); //mav.setViewName("getBoardList.do");
+		 * ==> ViewReslover 적용 전 mav.setViewName("redirect:getBoardList.do"); return
+		 * mav;
+		 */
 		
 	}
 
